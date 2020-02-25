d69a8905a83f7745ad33656ba5d20950f717ea38
Jason Bordeau <jason.bordeau.2021@student.lhschools.org>
2020-02-25 11:55:36 -0500

    intellij commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("+-------------------------------+");
+        System.out.println("|         Mami and Papi         |");
+        System.out.println("|          Gas Station          |");
+        System.out.println("|         Sale Receipts         |");
+        System.out.println("|                               |");
+        System.out.println("| 4000 Mink Street              |");
+        System.out.println("| Pataskala, OH 43062           |");
+        System.out.println("|                               |");
+        System.out.println("|                               |");
+        System.out.println("| Date            Time          |");
+        System.out.println("| 01/17/2020      2:20PM        |");
+        System.out.println("|                               |");
+        System.out.println("| Invoice#        745963        |");
+        System.out.println("| AUTH#                         |");
+        System.out.println("|          MASTERCARD           |");
+        System.out.println("|        ACCOUNT NUMBER         |");
+        System.out.println("|        **********8742         |");
+        System.out.println("|                               |");
+        System.out.println("| PUMP       PRODUCT       $/G  |");
+        System.out.println("|  01         UNLD        $2.59 |");
+        System.out.println("|                               |");
+        System.out.println("| GALLONS     FUEL        TOTAL |");
+        System.out.println("| 12.56          $        32.53 |");
+        System.out.println("|                               |");
+        System.out.println("|          THANK YOU!           |");
+        System.out.println("|         Mami and Papi         |");
+        System.out.println("|          Gas Station          |");
+        System.out.println("+-------------------------------+");
+
+
+
+
+    }
+}
